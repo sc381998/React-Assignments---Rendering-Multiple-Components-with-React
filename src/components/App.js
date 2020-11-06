457abe38fc9e90d44e3b35cc55827cef2ba6b670
Bishal Choudhary <bishalchoudhary67@gmail.com>
2020-11-06 22:45:45 +0530

    changes done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-ns-test="project-name">HealthySmile</h1>
+      <p data-ns-test="project-description">
+        It will be a ecommerce website where healthy, organic and ecofriendly
+        products will be available
+      </p>
+    </>
+  );
+};
 
 export default App;
